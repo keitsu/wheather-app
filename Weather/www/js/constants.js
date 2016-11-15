982a01d4bdfd32d1bee54ac57db212cdef3ed7f8
Samuel THOMAS <samuel@MacBook-Pro-de-Valentin.local>
2016-11-15 18:10:55 +0100

    ca marche pas encore

---
+++ Weather/www/js/constants.js
@@ -0,0 +1,8 @@
+angular.module('weather')
+
+.constant('OpenWeatherConfig', {
+   searchUrl: 'http://api.openweathermap.org/data/2.5/weather?q=',
+   units: '&units=metric',
+   appid: '&appid=6b76ca06ac88637b430047c1a960d5bc',
+   imgUrl: 'http://openweathermap.org/img/w/'
+})
