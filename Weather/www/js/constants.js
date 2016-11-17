b917680d4f649b8d719c1f5d0232011af2333995
Samuel THOMAS <samuel@MacBook-Pro-de-Valentin.local>
2016-11-17 15:00:27 +0100

    n'affiche rien, probleme de chargement du contenu

--- Weather/www/js/constants.js
+++ Weather/www/js/constants.js
@@ -1,8 +1,8 @@
-angular.module('weather')
+angular.module('weather',[])
 
 .constant('OpenWeatherConfig', {
    searchUrl: 'http://api.openweathermap.org/data/2.5/weather?q=',
    units: '&units=metric',
-   appid: '&appid=6b76ca06ac88637b430047c1a960d5bc',
+   appid: '&appid=<6b76ca06ac88637b430047c1a960d5bc>',
    imgUrl: 'http://openweathermap.org/img/w/'
 })
